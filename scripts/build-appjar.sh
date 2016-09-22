8dbf43604bcea93209354fa25c63e9816f37f514
Matt Russell <matt@mgrbyte.co.uk>
2016-09-22 14:17:08 +0100

    Pass path artefact into script + compile js.

--- scripts/build-appjar.sh
+++ scripts/build-appjar.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-artefact="target/app.jar"
-path=`lein with-profile +datomic-pro,+ddb ring uberjar | \
+lein_cmd_prefix="lein with-profile +datomic-pro,+ddb"
+artefact="$1"
+eval "${lein_cmd_prefix} cljsbuild once"
+path=`${lein_cmd_prefix} ring uberjar | \
 	    sed -n 's|^Created \(\/.*standalone.jar\)|\1|p'`
 
 mv "${path}" "${artefact}"
